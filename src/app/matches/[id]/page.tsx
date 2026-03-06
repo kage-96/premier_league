23defd6605bf6f0a1b602df3d0cd9c08637932ad
kage-96 <61382022+kage-96@users.noreply.github.com>
2026-03-07 01:12:45 +0900

    とりあえずUIはOK。あとはGoogleカレンダーとの連携。ついでに試合スケジュールの背景色も変更

--- src/app/matches/[id]/page.tsx
+++ src/app/matches/[id]/page.tsx
@@ -2,7 +2,23 @@
 import { useFetcher } from "@/app/_hooks/useFetcher";
 import { useParams } from "next/navigation";
 import { Matchtype } from "@/app/types/MatchType";
+import Image from "next/image";
 
+function formatJpTime(utc: string) {
+  return new Intl.DateTimeFormat("ja-JP", {
+    timeZone: "Asia/Tokyo",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(utc))
+}
+function toDateKey(utc:string){
+  const d = new Date(utc)
+  const jp = new Date(d.toLocaleString("en-US",{timeZone:"Asia/Tokyo"}))
+  const y = jp.getFullYear()
+  const m = String(jp.getMonth() + 1).padStart(2,"0")
+  const day = String(jp.getDate()).padStart(2,"0")
+  return `${y}-${m}-${day}`
+}
 export default function Matches(){
   const {id} = useParams()
 
@@ -12,5 +28,35 @@ export default function Matches(){
   if(error){return <div>Error: {error.message}</div>}
   if(!data){return <div>No data found</div>}
   console.log(data)
-  return <div>{JSON.stringify(data.score.winner)}</div>
+  return (
+    <div className="bg-gray-900 rounded-lg p-4">
+      <h2 className="text-center text-lg font-bold mb-4 bg-gray-800 p-2 rounded-md">{toDateKey(data.utcDate)}</h2>
+      <div className="grid grid-cols-[1fr_60px_1fr]">
+        <div className="flex items-center gap-2 text-right justify-end text-sm">
+          <p>{data.homeTeam.shortName}</p>
+          <Image src={data.homeTeam.crest} alt={data.homeTeam.tla} width={32} height={32} />
+        </div>
+        <div className="flex items-center gap-2 justify-center">
+          {data.status == 'FINISHED' ? (
+            <>
+              <p>{data.score.fullTime.home}</p>
+              <p>-</p>
+              <p>{data.score.fullTime.away}</p>
+            </>
+          ):(
+            <div className="flex items-center gap-2 justify-center">
+            <p>{formatJpTime(data.utcDate)}</p>
+          </div>
+          )}
+        </div>
+        <div className="flex items-center gap-2 text-left justify-start">
+          <Image src={data.awayTeam.crest} alt={data.awayTeam.tla} width={32} height={32} />
+          <p>{data.awayTeam.shortName}</p>
+        </div>
+      </div>
+      {data.status == 'SCHEDULED' || data.status == 'TIMED' && (
+        <button className="bg-blue-500 text-sm text-white px-4 py-2 rounded-md mx-auto block mt-4">カレンダーに登録</button>
+      )}
+    </div>
+  )
 }
